a974633225f0aec123aed0d252421a270b1c55db
Camilo Jimenez <camilo.jimenezc@gmail.com>
2019-08-20 00:34:14 -0500

    add main.go file

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	//"os/exec"
+	"fmt"
+)
+
+func main(){
+	fmt.Println("hello world")
+}
+
+//func runTest(){
+//
+//  go test .
+//}
+//  git commit -a
+//  git checkout HEAD main.go
+//
+//  inotifywait -r -e modify . >&2
+//  do_test && do_commit || do_revert
+// cbTerm := exec.Command("/bin/stty", cbreak, echo)    
+// cbTerm.Stdin = os.Stdin                              
+// err := cbTerm.Run()                                  
+
